d0ade9f572d5322847ad50021ef6e407df40fe62
Чернова Александра <estelelendil@mail.ru>
2022-07-22 14:16:36 +0300

    start working with class method

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 
+class Character{
+    constructor(name,type){
+        this.name = name;
+        this.type = type;
+    }
+    levelUp(){
+        if(this.health > 0){this.level +=1;
+        this.attack = this.attack * 1.2;
+        this.defence = ths.defence * 1.2;
+        this.health = 100;}
+    }
+    demage(points){
+        if(this.health > 0){
+            this.health -= points * (1 - defence / 100);
+        }
+    }
+}
